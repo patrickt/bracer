b76f7cb7498a7e75f2a6f3efe4397fe5a482ca43
Colin Barrett <colin@springsandstruts.com>
2014-05-04 02:44:32 -0700

    First pass at a testing infrastructure
    For now, we're going with hspec. It can run HUnit, QuickCheck and SmallCheck tests, and has a nice syntax.
    
    Tasty seems great but there's no working hspec integration package, and hacking one together was too much work for an afternoon. We can certainly use it or test-framework for our (forthcoming) golden tests separately, at least until there's a proper hspec runner for tasty.

---
+++ Language/Bracer/Test/C.hs
@@ -0,0 +1,39 @@
+module Language.Bracer.Test.C (tests) where
+
+  import Prelude ()
+  import Overture
+  
+  import Test.Hspec
+  import Test.HUnit
+  
+  import Control.Lens
+  import Text.Trifecta
+  import Data.Comp.Show
+  
+  import Language.Bracer
+  import Language.Bracer.Backends.C
+  
+  runCParser :: CParser a -> String -> Result a
+  runCParser p = parseString (unCParser p) mempty
+  
+  infix 1 `shouldParseAs`
+  shouldParseAs :: (ShowF a, EqF a, Functor a) => Result (Term a) -> Term a -> Assertion
+  shouldParseAs res ref = res `shouldSatisfy` (fromMaybe False . fmap (eqF ref) . preview _Success)
+  
+  tests :: Spec
+  tests = describe "C" $ do
+    describe "token parser" $ do
+      it "ignores traditional comments" $
+        (runCParser parseExpression "1 /* comment */") `shouldParseAs` (iIntLit 1 :: Term ExpressionSig)
+      it "ignores C++ style comments" $ 
+        (runCParser parseExpression "1 // comment") `shouldParseAs` (iIntLit 1 :: Term ExpressionSig)
+    describe "literal parser" $ do
+      it "parses integers" $
+        (runCParser parseExpression "1") `shouldParseAs` (iIntLit 1 :: Term ExpressionSig)
+      it "parses floats" $
+        (runCParser parseExpression "1.0") `shouldParseAs` (iFltLit 1.0 :: Term ExpressionSig)
+      it "parses characters" $
+        (runCParser parseExpression "'c'") `shouldParseAs` (iChrLit 'c' :: Term ExpressionSig)
+        
+      
+      
